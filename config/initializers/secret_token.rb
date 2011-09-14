f83d684cb74b6f0ac3f0316c1a2b7991c3d22fde
upender <upender@jasita.com>
2011-09-14 12:28:49 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smsgateway::Application.config.secret_token = '7cc490bfe144f55bb991cf4af11565ed9c757eeac12ad491c11922d74838cb45295b5ffdc85d320ad229046e7215293d508ba283ffc146df8cd0b07cda936b00'
