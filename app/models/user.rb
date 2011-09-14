bdff079b95721092aeae42cb1972b1e90c6fc082
upender <upender@jasita.com>
2011-09-14 15:03:38 +0530

    added validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :mobile_number, :address, :city, :state
+  validates :name, :email, :mobile_number, :address, :city, :state ,:presence => true
 end
